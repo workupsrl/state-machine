db11be785407bbaa02b19c8b3081134847fdbfc3
Paolo Pontarollo <paolo.pontarollo@neosperience.com>
2022-12-12 16:34:32 +0100

    Add trait state machines

--- src/Traits/HasStateMachines.php
+++ src/Traits/HasStateMachines.php
@@ -8,29 +8,26 @@ use SM\StateMachine\StateMachineInterface;
 
 trait HasStateMachines
 {
-    /** @var array $stateMachines */
-//    public array $stateMachines = [];
-
     protected static FactoryInterface $smFactory;
 
+    /** @noinspection PhpUnhandledExceptionInspection as requirements are always met */
     protected static function bootHasStateMachines()
     {
         static::$smFactory = app(FactoryInterface::class);
+        collect(static::$stateMachines)
+            ->keys()
+            ->each(fn($attribute) => static::$smFactory->addConfig(static::getStateMachineConfig($attribute), $attribute));
     }
 
-    /** @noinspection PhpUnhandledExceptionInspection as requirements are always met */
     protected function initializeHasStateMachines()
     {
-        collect($this->stateMachines)->keys()
-            ->each(fn ($attribute) => static::$smFactory->addConfig(
-                $this->getStateMachineConfig($attribute),
-                $attribute)
-            );
+        collect(static::$stateMachines)
+            ->keys()
+            ->each(fn($attribute) => $this->setInitialState($attribute));
     }
 
     /**
-     * @param  string  $attribute
-     *
+     * @param string $attribute
      * @return StateMachineInterface
      * @noinspection PhpUnhandledExceptionInspection as requirements are always met
      * @noinspection PhpDocMissingThrowsInspection as requirements are always met
@@ -41,31 +38,38 @@ trait HasStateMachines
     }
 
     /**
-     * @param  string  $attribute
-     *
+     * @param string $attribute
      * @return array
      */
-    public function getStateMachineConfig(string $attribute = 'state'): array
+    public static function getStateMachineConfig(string $attribute = 'state'): array
     {
-        $config = Arr::get($this->stateMachines, $attribute);
+        $config = Arr::get(static::$stateMachines, $attribute);
         if (is_string($config)) {
             $config = config($config);
         }
 
-        return $this->normalizeConfig($config, $attribute);
+        return static::normalizeConfig($config, $attribute);
     }
 
     /**
-     * @param  array  $config
+     * @param array $config
      * @param $attribute
-     *
      * @return array
      */
-    protected function normalizeConfig(array $config, $attribute): array
+    protected static function normalizeConfig(array $config, $attribute): array
     {
         return array_merge([
-            'class' => get_class($this),
+            'class' => get_called_class(),
             'property_path' => $attribute,
         ], $config);
+    }
+
+    protected function setInitialState(string $attribute)
+    {
+        if (!$state = Arr::get(static::$stateMachines, "$attribute.initial_state")) {
+            $state = Arr::get(static::getStateMachineConfig($attribute), 'states.0');
+        }
+
+        $this->setAttribute($attribute, $state);
     }
 }
